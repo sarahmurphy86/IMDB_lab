3cbec45cd0ea0dd35910a0a867c826789c438a54
Paul <rpmstevenson@gmail.com>
2018-04-26 15:46:07 +0100

    console input created

--- console.rb
+++ console.rb
@@ -2,3 +2,43 @@ require_relative('models/casting')
 require_relative('models/movie')
 require_relative('models/star')
 require( 'pry-byebug')
+
+star1 =Star.new({
+    'first_name' => 'Ryan',
+    'last_name' => 'Gosling'})
+star1.save()
+
+star2 =Star.new({
+    'first_name' => 'Brad',
+    'last_name' => 'Pitt'
+    })
+star2.save()
+
+movie1 =Movie.new({
+    'title' => 'Drive',
+    'genre' => 'Thriller',
+    'rating' => 5
+    })
+movie1.save()
+
+movie2 =Movie.new({
+    'title' => 'Burn after Reading',
+    'genre' => 'Comedy',
+    'rating' => 4
+    })
+movie2.save()
+
+casting1 = Casting.new({
+    'star_id' => star1.id, '
+    movie_id' => movie1.id
+    })
+casting1.save()
+
+casting2 = Casting.new({
+    'star_id' => star2.id, '
+    movie_id' => movie2.id
+    })
+casting1.save()
+
+binding.pry
+nil
--- db/imdb.sql
+++ db/imdb.sql
@@ -7,15 +7,16 @@ CREATE TABLE movies(
   title VARCHAR(255),
   genre VARCHAR(255),
   rating INT4
-)
+);
 
 CREATE TABLE stars(
   id SERIAL4 PRIMARY KEY,
   first_name VARCHAR(255),
   last_name VARCHAR(255)
-)
+);
+
 CREATE TABLE castings(
   id SERIAL4 PRIMARY KEY,
   star_id INT4 REFERENCES stars(id) ON DELETE CASCADE,
   movie_id INT4 REFERENCES movies(id) ON DELETE CASCADE
-)
+);
